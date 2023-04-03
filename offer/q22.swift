650552c1b47d6e7a66d8eb0761eee5ffde91a657
Chu Yong <chuyong155@hellobike.com>
2023-04-03 19:37:49 +0800

    [feat.] add q22 链表中倒数第k个节点

---
+++ offer/q22.swift
@@ -0,0 +1,70 @@
+//
+//  Solution.swift
+//  AlgorithmForHello
+//
+//  Created by Chu Yong on 4/3/23.
+//
+
+import Foundation
+
+/*
+https://leetcode.cn/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+
+剑指 Offer 22. 链表中倒数第k个节点
+
+输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+
+例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+
+ 
+
+示例：
+
+给定一个链表: 1->2->3->4->5, 和 k = 2.
+
+返回链表 4->5.
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func getKthFromEnd(_ head: ListNode?, _ k: Int) -> ListNode? {
+        var fast = head;
+        var slow = head;
+        for _ in 0 ..< k {
+            fast = fast?.next
+        }
+        while fast != nil {
+            fast = fast?.next
+            slow = slow?.next
+        }
+        return slow
+    }
+    
+    func test() {
+        getKthFromEnd(nil, 8)
+    }
+}
